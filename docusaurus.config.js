555ffc2e5cc85c2f1f965e241563d3ca6c81a3ac
kohheepeace <kohheepeace@gmail.com>
2020-02-02 23:51:14 +0700

    Disable Edit Github Link

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -73,7 +73,7 @@ module.exports = {
       {
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
-          editUrl: 'https://github.com/facebook/docusaurus/edit/master/website/',
+          editUrl: null,
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
