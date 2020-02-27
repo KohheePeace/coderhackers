7568c9b8ad162abc595b64be1341b3c2699ad0b3
kohheepeace <kohheepeace@gmail.com>
2020-02-27 07:23:56 +0700

    Update docusarus to alpha 0.43

--- src/theme/CodeBlock/index.js
+++ src/theme/CodeBlock/index.js
@@ -12,8 +12,11 @@ import defaultTheme from 'prism-react-renderer/themes/palenight';
 import Clipboard from 'clipboard';
 import rangeParser from 'parse-numeric-range';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import styles from './styles.module.css';
+import useThemeContext from '@theme/hooks/useThemeContext';
 import Prism from 'prism-react-renderer/prism';
+
+import styles from './styles.module.css';
+
 (typeof global !== 'undefined' ? global : window).Prism = Prism;
 require('prismjs/components/prism-dart');
 
@@ -25,10 +28,28 @@ export default ({children, className: languageClassName, metastring}) => {
       themeConfig: {prism = {}},
     },
   } = useDocusaurusContext();
+
   const [showCopied, setShowCopied] = useState(false);
+  const [mounted, setMounted] = useState(false);
+  // The Prism theme on SSR is always the default theme but the site theme
+  // can be in a different mode. React hydration doesn't update DOM styles
+  // that come from SSR. Hence force a re-render after mounting to apply the
+  // current relevant styles. There will be a flash seen of the original
+  // styles seen using this current approach but that's probably ok. Fixing
+  // the flash will require changing the theming approach and is not worth it
+  // at this point.
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
   const target = useRef(null);
   const button = useRef(null);
   let highlightLines = [];
+
+  const {isDarkTheme} = useThemeContext();
+  const lightModeTheme = prism.theme || defaultTheme;
+  const darkModeTheme = prism.darkTheme || lightModeTheme;
+  const prismTheme = isDarkTheme ? darkModeTheme : lightModeTheme;
 
   if (metastring && highlightLinesRangeRegex.test(metastring)) {
     const highlightLinesRange = metastring.match(highlightLinesRangeRegex)[1];
@@ -68,20 +89,27 @@ export default ({children, className: languageClassName, metastring}) => {
   return (
     <Highlight
       {...defaultProps}
-      theme={prism.theme || defaultTheme}
+      key={mounted}
+      theme={prismTheme}
       code={children.trim()}
       language={language}>
       {({className, style, tokens, getLineProps, getTokenProps}) => (
-        <div className={styles.codeBlockWrapper}>
-          <pre
-            ref={target}
-            className={classnames(className, styles.codeBlock)}
-            style={style}>
+        <pre className={classnames(className, styles.codeBlock)}>
+          <button
+            ref={button}
+            type="button"
+            aria-label="Copy code to clipboard"
+            className={styles.copyButton}
+            onClick={handleCopyCode}>
+            {showCopied ? 'Copied' : 'Copy'}
+          </button>
+
+          <code ref={target} className={styles.codeBlockLines} style={style}>
             {tokens.map((line, i) => {
               if (line.length === 1 && line[0].content === '') {
                 line[0].content = '\n'; // eslint-disable-line no-param-reassign
               }
-              
+
               const lineProps = getLineProps({line, key: i});
 
               if (highlightLines.includes(i + 1)) {
@@ -96,16 +124,8 @@ export default ({children, className: languageClassName, metastring}) => {
                 </div>
               );
             })}
-          </pre>
-          <button
-            ref={button}
-            type="button"
-            aria-label="Copy code to clipboard"
-            className={styles.copyButton}
-            onClick={handleCopyCode}>
-            {showCopied ? 'Copied' : 'Copy'}
-          </button>
-        </div>
+          </code>
+        </pre>
       )}
     </Highlight>
   );
